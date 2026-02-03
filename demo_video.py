fb283f0933c5015196a65fe139a196d79e5f3576
masonjfleming <mjf00015@mix.wvu.edu>
2026-02-03 16:24:44 -0500

    Update demo_video.py
    Input path updated to reflect our current work

--- demo_video.py
+++ demo_video.py
@@ -375,8 +375,11 @@ class VideoProcessorSK(VideoProcessor):
         self.vid.close()
 
 
-    
-input_path = '/content/Open-Pose-Keras/sample_videos'
+# Originial Input Path:    
+# input_path = '/content/Open-Pose-Keras/sample_videos'
+
+# New input path:
+input_path = '/content/drive/myDrive/colab_openpose/videos'
 keras_weights_file= '/content/Open-Pose-Keras/model/keras/model.h5'
 copy_to = '/content/drive/My Drive/colab_openpose/output_files/'
 
